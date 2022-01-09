22e73e23e27c6344e9d40c2c134216e162ff466c
LAPTOP-PATF7J21\Tibame_T14 <a1226667@yahoo.com.tw>
2022-01-09 16:26:29 +0800

    changer

--- src/main/java/test002.java
+++ src/main/java/test002.java
@@ -4,6 +4,9 @@ public class test002 {
 	public static void main(String[]args) {
 		
 		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
 		
 		
 	}
